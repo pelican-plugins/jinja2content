94aa66cd941de8e0a4598c78e033b0204736d5cb
Justin Mayer <entroP@gmail.com>
2020-02-23 18:57:27 +0100

    Apply Black and isort code formatting

--- pelican/plugins/jinja2content/jinja2content.py
+++ pelican/plugins/jinja2content/jinja2content.py
@@ -6,12 +6,14 @@ Pelican plugin that processes Markdown files as jinja templates.
 
 """
 
-from jinja2 import Environment, FileSystemLoader, ChoiceLoader
 import os
-from pelican import signals
-from pelican.readers import MarkdownReader, HTMLReader, RstReader
-from pelican.utils import pelican_open
 from tempfile import NamedTemporaryFile
+
+from jinja2 import ChoiceLoader, Environment, FileSystemLoader
+from pelican import signals
+from pelican.readers import HTMLReader, MarkdownReader, RstReader
+from pelican.utils import pelican_open
+
 
 class JinjaContentMixin:
     def __init__(self, *args, **kwargs):
@@ -19,25 +21,22 @@ class JinjaContentMixin:
 
         # will look first in 'JINJA2CONTENT_TEMPLATES', by default the
         # content root path, then in the theme's templates
-        local_dirs = self.settings.get('JINJA2CONTENT_TEMPLATES', ['.'])
-        local_dirs = [os.path.join(self.settings['PATH'], folder)
-                      for folder in local_dirs]
-        theme_dir = os.path.join(self.settings['THEME'], 'templates')
+        local_dirs = self.settings.get("JINJA2CONTENT_TEMPLATES", ["."])
+        local_dirs = [
+            os.path.join(self.settings["PATH"], folder) for folder in local_dirs
+        ]
+        theme_dir = os.path.join(self.settings["THEME"], "templates")
 
-        loaders = [FileSystemLoader(_dir) for _dir
-                   in local_dirs + [theme_dir]]
-        if 'JINJA_ENVIRONMENT' in self.settings: # pelican 3.7
-            jinja_environment = self.settings['JINJA_ENVIRONMENT']
+        loaders = [FileSystemLoader(_dir) for _dir in local_dirs + [theme_dir]]
+        if "JINJA_ENVIRONMENT" in self.settings:  # pelican 3.7
+            jinja_environment = self.settings["JINJA_ENVIRONMENT"]
         else:
             jinja_environment = {
-                'trim_blocks': True,
-                'lstrip_blocks': True,
-                'extensions': self.settings['JINJA_EXTENSIONS']
+                "trim_blocks": True,
+                "lstrip_blocks": True,
+                "extensions": self.settings["JINJA_EXTENSIONS"],
             }
-        self.env = Environment(
-            loader=ChoiceLoader(loaders),
-            **jinja_environment)
-
+        self.env = Environment(loader=ChoiceLoader(loaders), **jinja_environment)
 
     def read(self, source_path):
         with pelican_open(source_path) as text:
@@ -54,16 +53,20 @@ class JinjaContentMixin:
 class JinjaMarkdownReader(JinjaContentMixin, MarkdownReader):
     pass
 
+
 class JinjaRstReader(JinjaContentMixin, RstReader):
     pass
 
+
 class JinjaHTMLReader(JinjaContentMixin, HTMLReader):
     pass
+
 
 def add_reader(readers):
     for Reader in [JinjaMarkdownReader, JinjaRstReader, JinjaHTMLReader]:
         for ext in Reader.file_extensions:
             readers.reader_classes[ext] = Reader
+
 
 def register():
     signals.readers_init.connect(add_reader)
